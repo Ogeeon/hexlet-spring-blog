361cfeece7c2f7bfed6fffc391cb7c9b304675a2
Alexey Bogatyrev <avbogatyrev@gmail.com>
2026-02-09 19:49:27 +1000

    Add support for user management

--- app/src/main/java/io/hexlet/spring/PostsController.java
+++ app/src/main/java/io/hexlet/spring/PostsController.java
@@ -11,35 +11,37 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-public class PostController {
+@RequestMapping("/api/posts")
+public class PostsController {
     private final List<Post> posts = new ArrayList<>();
     private Long lastId = 0L;
 
-    @GetMapping("/posts")
+    @GetMapping
     public ResponseEntity<List<Post>> index(@RequestParam(defaultValue = "10") Integer limit) {
         var result = posts.stream().limit(limit).toList();
         return ResponseEntity.ok(result);
     }
 
-    @GetMapping("/posts/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Post> show(@PathVariable Long id) {
         var postOptional = posts.stream().filter(p -> p.getId().equals(id)).findFirst();
         return ResponseEntity.of(postOptional);
     }
 
-    @PostMapping("/posts")
+    @PostMapping
     public ResponseEntity<Post> create(@RequestBody Post post) {
         if (post.getTitle() == null || post.getTitle().isEmpty() ||
                 post.getContent() == null || post.getContent().isEmpty()) {
             ResponseEntity.badRequest().body("Post should have title and content");
+            return null;
         }
         post.setId(lastId++);
         post.setCreatedAt(LocalDateTime.now());
         posts.add(post);
-        return ResponseEntity.created(URI.create("/posts/" + post.getId())).body(post);
+        return ResponseEntity.created(URI.create("/" + post.getId())).body(post);
     }
 
-    @PutMapping("/posts/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Post> update(@PathVariable Long id, @RequestBody Post data) {
         if (data.getTitle() == null ||data.getTitle().isEmpty() ||
                 data.getContent() == null || data.getContent().isEmpty()) {
@@ -50,13 +52,16 @@ public class PostController {
             Post post = maybePost.get();
             post.setTitle(data.getTitle());
             post.setContent(data.getContent());
+            if (data.getAuthor() != null) {
+                post.setAuthor(data.getAuthor());
+            }
             return ResponseEntity.ok(data);
         } else {
             return ResponseEntity.notFound().build();
         }
     }
 
-    @DeleteMapping("/posts/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> destroy(@PathVariable Long id) {
         var removed = posts.removeIf(p -> p.getId().equals(id));
         return removed ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
